c5637de92d673b2dcb495c3a9ba4f68058a168bf
Christhoper Daniel Portillo Reyes <christhoper98_4@hotmail.com>
2024-08-31 16:33:28 -0600

    Cambios 20240831

--- src/main/java/com/christhoper/curso/springboot/webapp/springboot_web/SpringbootWebApplication.java
+++ src/main/java/com/christhoper/curso/springboot/webapp/springboot_web/SpringbootWebApplication.java
@@ -2,6 +2,7 @@ package com.christhoper.curso.springboot.webapp.springboot_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class SpringbootWebApplication {
--- src/main/java/com/christhoper/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/christhoper/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,19 +3,54 @@ package com.christhoper.curso.springboot.webapp.springboot_web.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.christhoper.curso.springboot.webapp.springboot_web.models.User;
 import com.christhoper.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+    // @Value("${config.message}")
+    // private String message;
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+    @Value("${config.code}")
+    private Integer code;
     
+    @Value("#{'${config.listOfValues}'.toUpperCase().split(',')}")
+    private List<String> valueList;
+    
+    @Value("#{'${config.listOfValues}'.toUpperCase()}")
+    private String valueString;
+
+    @Value("#{${config.valuesMap}}")
+    private Map<String, Object> valuesMap;
+    
+    @Value("#{${config.valuesMap}.product}")
+    private String product;
+
+    @Value("#{${config.valuesMap}.price}")
+    private Long price;
+
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
         
@@ -32,6 +67,29 @@ public class PathVariableController {
         json.put("product", product);
         json.put("id", id);
 
+        return json;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        user.setName(user.getName().toUpperCase());
+        return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message){
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("code", code);
+        json.put("message", message);
+        json.put("message2", environment.getProperty("config.message"));
+        json.put("code2", environment.getProperty("config.code", Long.class));
+        json.put("listOfValues", listOfValues);
+        json.put("valueList", valueList);
+        json.put("valueString", valueString);
+        json.put("valueMap", valuesMap);
+        json.put("product", product);
+        json.put("price", price);
         return json;
     }
 }
