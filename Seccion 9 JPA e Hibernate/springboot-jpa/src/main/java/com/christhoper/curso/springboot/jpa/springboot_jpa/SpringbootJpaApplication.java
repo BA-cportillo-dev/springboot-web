57c6fd7678eddeed3a8eb8a257dd88258c4f76ab
ChristhoperP <43559198+ChristhoperP@users.noreply.github.com>
2024-09-19 23:58:01 -0600

    Avances to video 127

--- Seccion 9 JPA e Hibernate/springboot-jpa/src/main/java/com/christhoper/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ Seccion 9 JPA e Hibernate/springboot-jpa/src/main/java/com/christhoper/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,6 +1,7 @@
 package com.christhoper.curso.springboot.jpa.springboot_jpa;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -22,7 +23,26 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		findOne();
+	}
+
+	public void findOne(){
+		// Person person = null;
+		// Optional<Person> optionalPerson = repository.findById(8L);
+		// if (optionalPerson.isPresent()) {
+		// 	person = optionalPerson.get();
+		// 	System.out.println(person);
+		// }else{
+		// 	System.out.println("No esta.");
+		// }
+		//repository.findById(1L).ifPresent(System.out::println);
+		//repository.findOne(1L).ifPresent(System.out::println);
+		//repository.findOneName("Pepe").ifPresent(System.out::println);
+		//repository.findOneLikeName("Pe").ifPresent(System.out::println);
+		repository.findByNameContaining("Pe").ifPresent(System.out::println);
+	}
+
+	public void list() {
 		//List<Person> persons = (List<Person>)repository.findAll();
 		//List<Person> persons = (List<Person>)repository.findByProgrammingLanguage("Java");
 		//List<Person> persons = (List<Person>)repository.buscarByProgrammingLanguage("Java", "Andres");
@@ -31,7 +51,10 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 		persons.stream().forEach(person -> {
 			System.out.println(person);
 		});
-
+		
+		List<Object[]> personsValues = repository.obtenerPersonData();
+		personsValues.stream().forEach(person -> {
+			System.out.println(person[0] + " es experto en " + person[1]);
+		});
 	}
-
 }
